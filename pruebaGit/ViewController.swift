b6ac1abc3a1ecb1c7b0b9e681f3a509836ea8deb
Juan David Ovalle Vega <juandavidovallevega@macbook-pro-de-juan.local>
2019-02-23 21:44:19 -0500

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Juan David Ovalle Vega on 2/23/19.
+//  Copyright © 2019 Juan David Ovalle Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
